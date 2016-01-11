072934d3babe954c7723409d31658315e05d0a96
cabiaom <cabiaom@seattleu.edu>
2016-01-10 20:38:05 -0800

    Commit to a new branch.

---
+++ Teleporter/a practice class.swift
@@ -0,0 +1,13 @@
+//
+//  a practice class.swift
+//  Teleporter
+//
+//  Created by Mark CABIAO on 1/10/16.
+//  Copyright © 2016 Mark CABIAO. All rights reserved.
+//
+
+import UIKit
+
+class a_practice_class: NSObject {
+
+}
